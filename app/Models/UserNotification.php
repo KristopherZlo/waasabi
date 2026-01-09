9beeae6bfd31ad4fad0a8726af38c4b91052d8ee
KristopherZlo <kostos2001@gmail.com>
2026-01-10 00:46:00 +0200

    add, model user notification

---
+++ app/Models/UserNotification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserNotification extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_notifications';
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'text',
+        'link',
+        'read_at',
+    ];
+
+    protected $casts = [
+        'read_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
