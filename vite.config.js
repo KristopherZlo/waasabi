f84b2351072fbbe44fb75e788f3e299e5a4d943c
KristopherZlo <kostos2001@gmail.com>
2026-01-27 19:04:00 +0200

    add, vite config

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/site-loader.ts', 'resources/css/app.css', 'resources/js/app.ts'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
