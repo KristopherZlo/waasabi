b757c7834c5dfb55d85b2ca79df8756b2b1b50f1
KristopherZlo <kostos2001@gmail.com>
2026-01-28 17:35:53 +0200

    feat: add comment request validation

---
+++ app/Http/Requests/StoreCommentRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCommentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $parentRules = ['nullable', 'integer'];
+        if (safeHasTable('post_comments')) {
+            $parentRules[] = 'exists:post_comments,id';
+        }
+
+        return [
+            'body' => ['required', 'string', 'max:2000'],
+            'section' => ['nullable', 'string', 'max:80'],
+            'parent_id' => $parentRules,
+        ];
+    }
+}
